b4a3d584f8e2008325be23ab42abb552d370d144
mcgrp <prasanth.bngrm46@gmail.com>
2023-06-03 10:32:05 +0530

    First commit

---
+++ src/Package1/Class1.java
@@ -0,0 +1,16 @@
+package Package1;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+		
+		int x =123;
+		int y= 234;
+		int sum;
+		sum= x+y;
+		System.out.println("Sum of x and y is:"+ sum);
+		
+
+	}
+
+}
